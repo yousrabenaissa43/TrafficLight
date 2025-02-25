0939d2a2a0dd1b6c5703a26e9a7cb7b484770a95
yousrabenaissa43 <yousrabenaissa43@gmail.com>
2025-02-25 22:54:41 +0100

    added street form

--- TrafficLight/Properties/Resources.Designer.cs
+++ TrafficLight/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace TrafficLight.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap street {
+            get {
+                object obj = ResourceManager.GetObject("street", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap yellow_ {
             get {
                 object obj = ResourceManager.GetObject("yellow ", resourceCulture);
